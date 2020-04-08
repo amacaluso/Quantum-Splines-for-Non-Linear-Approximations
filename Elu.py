7113987dba2f32192b4ef0684bcf855f1ed23eef
amacaluso <antonio.macaluso@studio.unibo.it>
2020-04-08 09:49:01 +0200

    QSline full and hybrid ok!

--- Elu.py
+++ Elu.py
@@ -15,16 +15,17 @@ def elu(z, c = 0, alpha = .3):
 x = np.arange(lower, upper + .03, step).tolist()
 y = [ elu(value) for value in x]
 
-range = (np.max(y) - np.min(y))
-y_norm = (y-np.min(y))/range
+# range = (np.max(y) - np.min(y))
+# y_norm = (y-np.min(y))/range
+# data_coef = coeff_splines_estimation(x, y_norm, label)
 
-data_coef = coeff_splines_estimation(x, y_norm, label)
+data_coef = coeff_splines_estimation(x, y, label)
 data_est = estimate_function(data_coef, elu, label, c = 0, step=step)
 
-data_est.full_quantum = data_est.full_quantum * range + np.min(y)
-data_est.hybrid_quantum = data_est.hybrid_quantum * range + np.min(y)
-data_est.classical_spline = data_est.classical_spline * range + np.min(y)
-data_est.to_csv('results/elu_estimates.csv', index = False)
+# data_est.full_quantum = data_est.full_quantum * range + np.min(y)
+# data_est.hybrid_quantum = data_est.hybrid_quantum * range + np.min(y)
+# data_est.classical_spline = data_est.classical_spline * range + np.min(y)
+# data_est.to_csv('results/elu_estimates.csv', index = False)
 
 plot_activation(label, data_est, data_coef, full = True)
 plot_activation(label, data_est, data_coef, full = False)
--- Relu.py
+++ Relu.py
@@ -9,7 +9,7 @@ step = .1
 ## Definition of the interval of B-Spline
 
 label = 'relu'
-def relu(x, c = 0):
+def relu(x, c = 0.1):
     return c + max(0.0, x)
 
 x = np.arange(lower, upper + .03, step).tolist()
@@ -18,10 +18,10 @@ y = [ relu(value) for value in x]
 data_coef = coeff_splines_estimation(x, y, label) # data_coef = pd.read_csv('results/relu_full.csv')
 data_est = estimate_function(data_coef, relu, label, c = 0, step=step)
 
-data_est.hybrid_quantum = data_est.hybrid_quantum
-data_est.classical_spline = data_est.classical_spline
+# data_est.hybrid_quantum = data_est.hybrid_quantum
+# data_est.classical_spline = data_est.classical_spline
+# data_est.to_csv('results/relu_estimates.csv')
 
-data_est.to_csv('results/relu_estimates.csv')
 plot_activation(label, data_est, data_coef, full = True)
 plot_activation(label, data_est, data_coef, full = False)
 
