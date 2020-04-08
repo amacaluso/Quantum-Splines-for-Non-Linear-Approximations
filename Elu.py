2d1be04222ecf1243ff2d8ff64ed15ac1f0365bd
amacaluso <antonio.macaluso@studio.unibo.it>
2020-04-08 11:39:10 +0200

    QSline full and hybrid ok!

--- Elu.py
+++ Elu.py
@@ -7,13 +7,13 @@ step = .1
 
 
 ## Definition of the interval of B-Spline
-
+c = .3
 label = 'elu'
 def elu(z, c = 0, alpha = .3):
 	return c + z if z >= 0 else c + alpha*(e**z -1)
 
 x = np.arange(lower, upper + .03, step).tolist()
-y = [ elu(value) for value in x]
+y = [ elu(value, c) for value in x]
 
 # range = (np.max(y) - np.min(y))
 # y_norm = (y-np.min(y))/range
@@ -26,6 +26,11 @@ data_est = estimate_function(data_coef, elu, label, c = 0, step=step)
 # data_est.hybrid_quantum = data_est.hybrid_quantum * range + np.min(y)
 # data_est.classical_spline = data_est.classical_spline * range + np.min(y)
 # data_est.to_csv('results/elu_estimates.csv', index = False)
+
+data_est.hybrid_quantum = data_est.hybrid_quantum - c
+data_est.classical_spline = data_est.classical_spline - c
+data_est.to_csv('results/' + label + '_estimates.csv', index=False)
+
 
 plot_activation(label, data_est, data_coef, full = True)
 plot_activation(label, data_est, data_coef, full = False)
--- Relu.py
+++ Relu.py
@@ -7,20 +7,20 @@ step = .1
 
 
 ## Definition of the interval of B-Spline
-
+c=.5
 label = 'relu'
-def relu(x, c = 0.1):
+def relu(x, c = 0):
     return c + max(0.0, x)
 
 x = np.arange(lower, upper + .03, step).tolist()
-y = [ relu(value) for value in x]
+y = [ relu(value, c) for value in x]
 
 data_coef = coeff_splines_estimation(x, y, label) # data_coef = pd.read_csv('results/relu_full.csv')
-data_est = estimate_function(data_coef, relu, label, c = 0, step=step)
+data_est = estimate_function(data_coef, relu, label, c=0, step=step)
 
-# data_est.hybrid_quantum = data_est.hybrid_quantum
-# data_est.classical_spline = data_est.classical_spline
-# data_est.to_csv('results/relu_estimates.csv')
+data_est.hybrid_quantum = data_est.hybrid_quantum - c
+data_est.classical_spline = data_est.classical_spline - c
+data_est.to_csv('results/' + label + '_estimates.csv', index=False)
 
 plot_activation(label, data_est, data_coef, full = True)
 plot_activation(label, data_est, data_coef, full = False)
