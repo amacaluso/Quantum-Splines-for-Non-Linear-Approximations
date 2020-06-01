fb0674fc2b9cba31d941b2ca2eac2be658e89f15
Luca Clissa <luca.clissa2@unibo.it>
2020-06-02 00:09:18 +0200

    Add copyright header to pythons scripts.

--- Viz_complexity.py
+++ Viz_complexity.py
@@ -1,3 +1,16 @@
+# Copyright 2020 Antonio Macaluso
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# TODO refactor *viz* scripts with meaningful names and update the README
+
 # Reference CG, qubic spline, equazione 6 equazione paragrafo 1
 
 # to do: mettere il grafico sul condition num in funzione di lambda
